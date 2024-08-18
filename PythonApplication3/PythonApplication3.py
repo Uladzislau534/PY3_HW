ba29d706fdfaaea6e472251f30ddb59a345be2fd
Uladzislau Balan <vladislav.balan96@gmail.com>
2024-08-18 20:29:56 +0300

    3_end

--- PythonApplication3/PythonApplication3.py
+++ PythonApplication3/PythonApplication3.py
@@ -1,1 +1,19 @@
 
+n = input("Введите номер билета (шестизначный): ")
+
+
+if len(n) == 6 and n.isdigit():
+
+    first_half = n[:3]
+    second_half = n[3:]
+
+    sum_first_half = sum(int(digit) for digit in first_half)
+    sum_second_half = sum(int(digit) for digit in second_half)
+
+    if sum_first_half == sum_second_half:
+        print("yes")
+    else:
+        print("no")
+else:
+    print("Неверный ввод. Пожалуйста, введите шестизначный номер билета.")
+
